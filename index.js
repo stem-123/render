530eacfbb68612316ccb7f00599c3328f83e8051
stem-123 <stemcomputer2025@gmail.com>
2025-06-02 16:53:46 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === 'ping') {
+    message.channel.send('pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
